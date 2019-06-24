544c02267737364857e7618a80c25f41d64299d6
Sule, Nishant V <nsule@seas.harvard.edu>
2019-06-24 13:50:46 -0400

    Update AudioFP.py

--- AudioFP.py
+++ AudioFP.py
@@ -9,6 +9,7 @@ import pyaudio
 import warnings
 import sys
 import pickle 
+# import hashlib
 
 # This class defines an AudioFP object that stores the name a song and its fingerprint. 
 # It also contains all the functions used to read/record audio, generate spectrogram, find peaks,
@@ -34,7 +35,7 @@ class AudioFP():
         self.songname = ''
         self.fingerprint = datasketch.MinHash(num_perm=256)
         self.framerate = []
-        if input('Do you want to proceed manually? Enter "y" or "n": ') == 'n':
+        if input('Enter "a" for automated fingerprinting or "m" to proceed manually: ') == 'a':
             self.ask_user()
         else:
             pass
@@ -59,12 +60,11 @@ class AudioFP():
                 print('Not saving anything')
         elif audio_type == 'r':
             filename = input('Enter a name for the recording:')
-            self.songname = filename  
             if input('Do you want to show all plots? Enter "y" or "n": ') == 'y':
                 plot = True
             else:
                 plot = False
-            channels, self.framerate = self.record_audiofile(plot)
+            channels, self.framerate = self.record_audiofile(plot, filename)
             f, t, sgram = self.generate_spectrogram(plot, channels, self.framerate)
             fp, tp, peaks = self.find_peaks(plot, f, t, sgram)
             self.generate_fingerprint(plot, fp, tp, peaks)
@@ -114,9 +114,10 @@ class AudioFP():
         return channels, framerate
             
     # Record audio file using pyaudio and plot signal
-    def record_audiofile(self, plot):
+    def record_audiofile(self, plot, filename):
         rec_time = int(input('How long do you want to record? Enter time in seconds: '))
         start_rec = input('Do you want to start recoding? Enter "y" to start:')
+        self.songname = filename  
         if start_rec=='y':
             chk_size = 8192  # chunk size
             fmt = pyaudio.paInt16  # format of audio 
@@ -210,6 +211,24 @@ class AudioFP():
         if plot:
             print('{} audio-fingerprint: '.format(self.songname))
             print(self.fingerprint.digest())
+            
+#     def generate_fingerprint(self, plot, fp, tp, peaks):
+#         # Create the data to be used for fingerprinting
+#         # for each frequency (anchor) find the next few frequencies (targets) and calculate their time deltas
+#         # the anchor-target frequency pairs and their time deltas will be used to generate the fingerprints
+#         s = []  # Empty list to contain data for fingerprint
+#         for i in range(len(peaks)):
+#             for j in range(1, peak_connectivity):
+#                 if (i + j) < len(peaks):
+#                     f1 = fp[i]
+#                     f2 = fp[i + j]
+#                     t1 = tp[i]
+#                     t2 = tp[i + j]
+#                     t_delta = t2 - t1
+#                     if t_delta >= peak_time_delta_min and t_delta <= peak_time_delta_max:
+#                         h = hashlib.sha1((str(f1) + str(f2) + str(t_delta)).encode('utf8'))
+#                         s.append([h.hexdigest(), t1])
+#         return s
     
     # Save the AudioFP object to file for later use
     def save_fingerprint(self):
